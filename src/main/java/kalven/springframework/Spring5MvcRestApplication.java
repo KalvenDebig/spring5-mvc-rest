8f1647deeb184de7987a1f7cade5b74e7001b071
Sijie Shen <shen.sij@northeastern.edu>
2023-02-16 21:30:16 -0500

    added @entity to category, retry bringing up application context

--- src/main/java/kalven/springframework/Spring5MvcRestApplication.java
+++ src/main/java/kalven/springframework/Spring5MvcRestApplication.java
@@ -1,4 +1,4 @@
-package kalven.springframework.spring5mvcrest;
+package kalven.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/kalven/springframework/domain/Category.java
+++ src/main/java/kalven/springframework/domain/Category.java
@@ -1,5 +1,6 @@
 package kalven.springframework.domain;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -10,6 +11,7 @@ import lombok.Data;
  * @Author kalvens on 2/16/23
  */
 @Data
+@Entity
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
