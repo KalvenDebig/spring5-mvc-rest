f1e6178a72b7b246063e701a51e0c61136f7890c
Sijie Shen <shen.sij@northeastern.edu>
2023-02-16 21:28:19 -0500

    resolve circle ci build failed

--- src/main/java/kalven/springframework/spring5mvcrest/Spring5MvcRestApplication.java
+++ src/main/java/kalven/springframework/spring5mvcrest/Spring5MvcRestApplication.java
@@ -1,4 +1,4 @@
-package kalven.springframework;
+package kalven.springframework.spring5mvcrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
